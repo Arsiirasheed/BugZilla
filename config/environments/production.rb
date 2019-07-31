e3f2e7f64b83f2560409d2f7c40be02ceec352c8
Arslan Asif <munnakhan0101@gmail.com>
2019-07-31 17:47:06 +0500

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,6 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.action_mailer.default_url_options = { :host => "https://bugzilla-demo.herokuapp.com/" }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
