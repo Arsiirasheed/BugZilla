24b665dd08849eb1a13236729da5d6d97074203b
Arslan Asif <munnakhan0101@gmail.com>
2019-07-31 17:18:13 +0500

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.action_mailer.default_url_options = { :host => "https://bugzilla-demo.herokuapp.com/" }
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
